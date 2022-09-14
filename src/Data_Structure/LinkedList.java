0693640ddcf35a30aa23ebea7e154fbebf5537c4
usha-12 <usha12ahirwar@gmail.com>
2022-09-14 18:16:18 +0530

    Ability to create Linked List by appending 30 and 70 to 56

--- src/Data_Structure/LinkedList.java
+++ src/Data_Structure/LinkedList.java
@@ -49,9 +49,9 @@ public void printLinkedList(LinkedList list){
 
 
 /*Ability to create Linked
-List by adding 30 and 56
-to 70
-- Node with data 70 is First Created
-- Next 30 is added to 70
-- Finally 56 is added to 30
+List by appending 30 and
+70 to 56
+- Node with data 56 is First Created
+- Next Append 30 to 56
+- Finally Append 70 to 30
 - LinkedList Sequence: 56->30->70*/
