292bf8bd2ff5140c59b110beb3eb1db8d64320fd
usha-12 <usha12ahirwar@gmail.com>
2022-09-14 17:45:30 +0530

    Lets create a simple Linked List of 56, 30 and 70

--- src/Data_Structure/LinkedList.java
+++ src/Data_Structure/LinkedList.java
@@ -3,5 +3,6 @@ package Data_Structure;
 public class LinkedList {
     public static void main(String[] args) {
         System.out.println("LinkedList:");
+        System.out.println("Lets create a simple Linked List of:\n 56, 30, 70");
     }
 }
